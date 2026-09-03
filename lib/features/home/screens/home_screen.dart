322105855d81ce32d58aaff2c7bbb99386c54cde
Noah Michiel <noahmichiel@Noahs-MacBook-Air.local>
2026-09-03 15:52:27 +0530

    frame-app-rdy3

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -18,7 +18,7 @@ class HomeScreen extends StatelessWidget {
       body: SafeArea(
         child: Center(
           child: Text(
-            'Hey, where were you?',
+            'Afternoon',
             style: textTheme.titleLarge,
           ),
         ),
